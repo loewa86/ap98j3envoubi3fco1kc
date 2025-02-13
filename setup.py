f5437b898fdd1b0ddfb393ec34869570d837087f
loewa86 <56171670+loewa86@users.noreply.github.com>
2025-02-13 16:57:15 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="ap98j3envoubi3fco1kc",
     version="0.0.73",
     packages=find_packages(),
-    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp"],
+    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "aiohttp_socks"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
